6d3d58f732ab84e6d8dfce6254cd15e7e6644adb
Baptiste LABAT <baptiste.labat@gmail.com>
2024-10-30 21:48:00 +0100

    Fix installation

--- install_mamba.sh
+++ install_mamba.sh
@@ -1,13 +1,16 @@
-
+#!/bin/bash
 # pseudocode to check if mamba is installed
 #if conda list
 
 # https://github.com/conda-forge/miniforge
-
-wget -O Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-bash Miniforge3.sh -b -p "${HOME}/conda"
-
-rm Miniforge3.sh
+if conda info; then
+  echo " --- conda already installed"
+else
+  echo " --- installing conda"
+  wget -O Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+  bash Miniforge3.sh -b -p "${HOME}/conda"
+  rm Miniforge3.sh
+fi
 
 source "${HOME}/conda/etc/profile.d/conda.sh"
 # For mamba support also run the following command
--- makefile
+++ makefile
@@ -1,3 +1,6 @@
+.PHONY: help
+.DEFAULT_GOAL := help
+
 install-mamba: ## install mamba (conda replacement)
 	./install_mamba.sh
 
@@ -5,8 +8,12 @@ build-env: ## Setup environment (only when updating python or poetry version)
 	conda-lock -f environment.yml -p osx-64 -p linux-64
 
 create-ailedeau: ## Create copy of environment
-	mamba env remove --name ailedeu
-	conda-lock install --name ailedeau conda-lock.yml
+	@if conda env list | grep -q ailedeau; then \
+		conda env remove --name ailedeau; \
+	else \
+		echo "Environment 'ailedeau' does not exist, creating."; \
+	fi
+	${HOME}/conda/bin/conda-lock install --name ailedeau conda-lock.yml
 
 install-poetry: ## Be sure to activate env
 	poetry install
