463990856277571c7beadd4fbff765da34cfc3ae
Baptiste LABAT <baptiste.labat@gmail.com>
2025-03-09 23:54:10 +0100

    working code but without contact

--- src/braiding_simulation/braid.py
+++ src/braiding_simulation/braid.py
@@ -1,95 +1,145 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# This is an EXUDYN example
+#
+# Details:  ANCFCable test with many cable elements
+#
+# Author:   Johannes Gerstmayr
+# Date:     2023-10-15
+#
+# Copyright:This file is part of Exudyn. Exudyn is free software. You can redistribute it and/or modify it under the terms of the Exudyn license. See 'LICENSE.txt' for more details.
+#
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 import exudyn as exu
-from exudyn.itemInterface import *
+from exudyn.utilities import *  # includes itemInterface and rigidBodyUtilities
+import exudyn.graphics as graphics  # only import if it does not conflict
+from exudyn.beams import *
+
 import numpy as np
 
+# create an environment for mini example
 SC = exu.SystemContainer()
 mbs = SC.AddSystem()
 
-# Background for the ground object (optional for visualization)
-rect = [-2, -2, 4, 2]  # xmin, ymin, xmax, ymax
-background0 = {'type': 'Line', 'color': [0.1, 0.1, 0.8, 1],
-               'data': [rect[0], rect[1], 0, rect[2], rect[1], 0, rect[2], rect[3], 0, rect[0], rect[3], 0, rect[0],
-                        rect[1], 0]}  # Background
-background1 = {'type': 'Line', 'color': [0.1, 0.1, 0.8, 1], 'data': [0, -1, 0, 2, -1, 0]}  # Background
-oGround = mbs.AddObject(
-    ObjectGround(referencePosition=[0, 0, 0], visualization=VObjectGround(graphicsData=[background0, background1])))
+oGround = mbs.AddObject(ObjectGround(referencePosition=[0, 0, 0]))
+nGround = mbs.AddNode(NodePointGround(referenceCoordinates=[0, 0, 0]))
 
-# Cable parameters
-L = 2  # Length of each ANCF element in meters
-E = 2.07e11  # Young's modulus of ANCF element in N/m^2
-rho = 7800  # Density of ANCF element in kg/m^3
-b = 0.001  # Width of rectangular ANCF element in meters
-h = 0.001  # Height of rectangular ANCF element in meters
-A = b * h  # Cross-sectional area in m^2
-I = b * h ** 3 / 12  # Second moment of area in m^4
-f = 3 * E * I / L ** 2  # Tip load applied to the ANCF element in N
+rhoA = 78.
+EA = 100000.
+EI = 200
+length = 20
 
-# Number of elements per cable
-nElements = 10  # Number of elements per strand (more elements will make the simulation finer)
+nCables = 3
+for i in range(nCables):
+    p0 = np.array([0, i, 0])
+    p1 = p0 + [length, 0, 0]
 
-# Create cables and nodes for 3 strands (braiding)
-cableList = []  # List of cable objects
-nodeList = []  # List of nodes for the cables
+    cable = ObjectANCFCable(
+        physicsLength=length,
+        physicsMassPerLength=rhoA,
+        physicsBendingStiffness=EI * (0.5 + np.random.rand() * 0.25),
+        physicsBendingDamping=200000,
+        physicsAxialStiffness=EA,
+        physicsAxialDamping=200000
+    )
 
-# Create nodes for each strand and add cable elements
-for strand_idx in range(3):  # Create 3 strands (adjust number as needed)
-    nodeListForStrand = []  # Nodes for each strand
-    nc0 = mbs.AddNode(Point2DS1(referenceCoordinates=[0.5 * strand_idx, 0, 1, 0]))  # Starting point of the strand
-    nodeListForStrand.append(nc0)
+    ancf = GenerateStraightLineANCFCable(mbs=mbs,
+                                         positionOfNode0=p0, positionOfNode1=p1,
+                                         numberOfElements=12,  # converged to 4 digits
+                                         cableTemplate=cable,  # this defines the beam element properties
+                                         massProportionalLoad=[0, 0, 0],
+                                         fixedConstraintsNode0=[1, 1, 1, 0, 1, 1],
+                                         # add constraints for pos and rot (r'_y,r'_z)
+                                         )
+    ancfNodes = ancf[0]
+    node = ancfNodes[-1]
+    mANCF0 = mbs.AddMarker(MarkerNodePosition(nodeNumber=node))
+    if i == 1:
+        def braiding(mbs, t: float, loadVector: np.ndarray) -> np.ndarray:
+            """
+            Wrapper function to apply aerodynamic forces via wind_wrench.
 
-    # Add cable elements along the length of each strand
-    lElem = L / nElements
-    for i in range(nElements):
-        nLast = mbs.AddNode(Point2DS1(referenceCoordinates=[lElem * (i + 1) + 0.5 * strand_idx, 0, 1, 0]))
-        nodeListForStrand.append(nLast)
-        # Add the Cable2D element between nodes
-        elem = mbs.AddObject(Cable2D(physicsLength=lElem, physicsMassPerLength=rho * A,
-                                     physicsBendingStiffness=E * I, physicsAxialStiffness=E * A,
-                                     nodeNumbers=[int(nc0) + i, int(nc0) + i + 1]))
-        cableList.append(elem)
+            Args:
+                mbs: Multi-body system instance.
+                t: Current time in the simulation.
+                loadVector: Placeholder (not used).
 
-    # Add the starting and ending markers
-    mANCF0 = mbs.AddMarker(MarkerNodeCoordinate(nodeNumber=nc0, coordinate=0))
-    mANCF1 = mbs.AddMarker(MarkerNodeCoordinate(nodeNumber=nc0, coordinate=1))
-    mANCF2 = mbs.AddMarker(MarkerNodeCoordinate(nodeNumber=nc0, coordinate=3))
-    mbs.AddObject(CoordinateConstraint(markerNumbers=[oGround, mANCF0]))
-    mbs.AddObject(CoordinateConstraint(markerNumbers=[oGround, mANCF1]))
-    mbs.AddObject(CoordinateConstraint(markerNumbers=[oGround, mANCF2]))
+            Returns:
+                np.ndarray: The computed force or torque for the specific context.
+            """
+            T = 10
+            return [0, 100 * np.sin(2 * np.pi * t / T + i * np.pi / 2), 100 * np.cos(2 * np.pi * t / T + i * np.pi / 2)]
 
-# Add gravity to the nodes
-for i in range(len(nodeList)):
-    m = mbs.AddMarker(MarkerNodePosition(nodeNumber=nodeList[i]))
-    fact = 1  # Add (half) weight of two elements to node
-    if (i == 0) | (i == len(nodeList) - 1):
-        fact = 0.5  # First and last node only weighted half
-    mbs.AddLoad(Force(markerNumber=m, loadVector=[0, -40 * 2 * rho * A * fact * lElem, 0]))  # Weight due to gravity
 
+        mbs.AddLoad(Force(markerNumber=mANCF0, loadVectorUserFunction=braiding))
+    else:
+        def braiding1(mbs, t: float, loadVector: np.ndarray) -> np.ndarray:
+            """
+            Wrapper function to apply aerodynamic forces via wind_wrench.
+
+            Args:
+                mbs: Multi-body system instance.
+                t: Current time in the simulation.
+                loadVector: Placeholder (not used).
+
+            Returns:
+                np.ndarray: The computed force or torque for the specific context.
+            """
+            T = 10
+            return [0, 100 * np.sin(-2 * np.pi * t / T + i * np.pi / 2),
+                    100 * np.cos(-2 * np.pi * t / T + i * np.pi / 2)]
+
+
+        mbs.AddLoad(Force(markerNumber=mANCF0, loadVectorUserFunction=braiding1))
 # Contact between the cables: You can add contact between cables if needed
-cStiffness = 1e3
-cDamping = 0.02 * cStiffness
-for i in range(len(cableList)):
-    # Define contact between cables (use an actual contact model here)
-    mCable = mbs.AddMarker(MarkerBodyCable2DShape(bodyNumber=cableList[i], numberOfSegments=nElements))
-    # Example: Create simple "contact" at specific cable intersections or a ground interaction
-    if i == 0:
-        mbs.AddObject(ObjectContactCoordinate(markerNumbers=[oGround, mCable],
-                                              contactStiffness=cStiffness, contactDamping=cDamping, offset=-0.2))
-
-# Assemble the system
+# cStiffness = 1e3
+# cDamping = 0.02 * cStiffness
+# for i in range(len(cableList)):
+#     # Define contact between cables (use an actual contact model here)
+#     mCable = mbs.AddMarker(MarkerBodyCable2DShape(bodyNumber=cableList[i], numberOfSegments=nElements))
+#     # Example: Create simple "contact" at specific cable intersections or a ground interaction
+#     # if i == 0:
+#     #     mbs.AddObject(ObjectContactCoordinate(markerNumbers=[oGround, mCable],
+#     #
+# assemble and solve system for default parameters
 mbs.Assemble()
 
-# Simulation settings
-simulationSettings = exu.SimulationSettings()
-simulationSettings.timeIntegration.numberOfSteps = 10000
-simulationSettings.timeIntegration.endTime = 0.05
-simulationSettings.solutionSettings.writeSolutionToFile = True
-simulationSettings.displayComputationTime = True
-simulationSettings.timeIntegration.verboseMode = 1
-simulationSettings.timeIntegration.newton.relativeTolerance = 1e-8
-simulationSettings.timeIntegration.newton.absoluteTolerance = 1e-10
+endTime = 100
+stepSize = 0.5e-2
 
-# Run the simulation
+simulationSettings = exu.SimulationSettings()
+
+# simulationSettings.solutionSettings.writeSolutionToFile = False
+simulationSettings.solutionSettings.solutionWritePeriod = 0.02  # data not used
+simulationSettings.solutionSettings.binarySolutionFile = True
+simulationSettings.solutionSettings.outputPrecision = 6  # float
+simulationSettings.solutionSettings.sensorsWritePeriod = 0.002  # data not used
+simulationSettings.timeIntegration.verboseMode = 1  # turn off, because of lots of output
+simulationSettings.linearSolverType = exu.LinearSolverType.EigenSparse
+simulationSettings.parallel.numberOfThreads = 8
+simulationSettings.displayComputationTime = True
+simulationSettings.displayStatistics = True
+
+simulationSettings.timeIntegration.numberOfSteps = int(endTime / stepSize)
+simulationSettings.timeIntegration.endTime = endTime
+simulationSettings.timeIntegration.newton.useModifiedNewton = True
+
+# simulationSettings.timeIntegration.simulateInRealtime = True
+# simulationSettings.timeIntegration.realtimeFactor = 0.5
+
+SC.visualizationSettings.general.graphicsUpdateInterval = 0.02
+SC.visualizationSettings.window.renderWindowSize = [1200, 1024]
+SC.visualizationSettings.nodes.show = False
+SC.visualizationSettings.loads.show = False
+SC.visualizationSettings.connectors.show = False
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 exu.StartRenderer()
+# mbs.WaitForUserToContinue()
+
 mbs.SolveDynamic(simulationSettings)
+
 SC.WaitForRenderEngineStopFlag()
-exu.StopRenderer()
+exu.StopRenderer()  # safely close rendering window!
+
+# mbs.SolutionViewer()
